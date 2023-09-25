cb0e59fdbff55ef3a921e309ab9ca9d68a6d2321
tushar-454 <imtushar454@gmail.com>
2023-09-25 10:42:46 +0600

    if donationlistcard more then 4 then show see more button added and click see more then show all donation list card and see more button hide function added

--- src/Components/Donations/DonationsList.jsx
+++ src/Components/Donations/DonationsList.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import DonationListCardDetails from './DonationListCardDetails';
 
 const Donations = () => {
   const [donateCards, setDonateCards] = useState([]);
+  const [seemore, setSeeMore] = useState(false);
+  const [slice, setSlice] = useState(4);
   const donations = useLoaderData();
   const donateId = JSON.parse(localStorage.getItem('donateId'));
   const donateIdList = donations.filter((donation) =>
@@ -12,7 +15,10 @@ const Donations = () => {
   useEffect(() => {
     setDonateCards(donateIdList);
   }, []);
-  console.log(donateIdList);
+  const handleSeeMore = () => {
+    setSeeMore(!seemore);
+    setSlice(donateIdList.length);
+  };
   return (
     <div>
       <div className='container mx-auto px-4'>
@@ -26,13 +32,34 @@ const Donations = () => {
           ) : (
             <>
               <div className='grid gap-5 grid-cols-1 lg:grid-cols-2'>
-                {donateCards.map((listCardDetails) => (
-                  <DonationListCardDetails
-                    key={listCardDetails.id}
-                    listCardDetails={listCardDetails}
-                  />
-                ))}
+                {seemore
+                  ? donateCards
+                      .slice(0, slice)
+                      .map((listCardDetails) => (
+                        <DonationListCardDetails
+                          key={listCardDetails.id}
+                          listCardDetails={listCardDetails}
+                        />
+                      ))
+                  : donateCards
+                      .slice(0, slice)
+                      .map((listCardDetails) => (
+                        <DonationListCardDetails
+                          key={listCardDetails.id}
+                          listCardDetails={listCardDetails}
+                        />
+                      ))}
               </div>
+              {donateCards.length > 4 && !seemore && (
+                <div className='seemorebtn my-5 text-center'>
+                  <button
+                    onClick={handleSeeMore}
+                    className='text-white bg-tertiary font-semibold text-lg px-4 py-2 rounded-lg transition active:bg-[#056a34]'
+                  >
+                    See All
+                  </button>
+                </div>
+              )}
             </>
           )}
         </div>
