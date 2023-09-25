926d5f1b65a8cee2554c0bdad62e11c60e1f0e32
tushar-454 <imtushar454@gmail.com>
2023-09-25 11:35:42 +0600

    pic chart added on statistics page which show my donation on number of percentage compair with total donation.

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,5 +1,78 @@
+import { Cell, Pie, PieChart } from 'recharts';
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill='white'
+      textAnchor={x > cx ? 'start' : 'end'}
+      dominantBaseline='central'
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
 const Statistics = () => {
-  return <div>this is a Statistics section</div>;
+  let donateIdLength = 0;
+  if (localStorage.getItem('donateId')) {
+    donateIdLength = JSON.parse(localStorage.getItem('donateId')).length;
+  }
+  const data = [
+    {
+      name: 'Total Donation',
+      value: 12 - donateIdLength,
+    },
+    {
+      name: 'Your Donation',
+      value: donateIdLength,
+    },
+  ];
+
+  const COLORS = ['#FF444A', '#00C49F'];
+
+  return (
+    <div className='flex flex-col items-center'>
+      <PieChart width={400} height={400}>
+        <Pie
+          data={data}
+          cx={200}
+          cy={200}
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={150}
+          fill='#8884d8'
+          dataKey='value'
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+      <div className='colorIndicator flex flex-col sm:flex-row items-center justify-center gap-10'>
+        <h1 className='text-xl flex gap-5 items-center'>
+          Your Donation{' '}
+          <span className='w-24 h-2 bg-[#00C49F] block rounded'></span>
+        </h1>
+        <h1 className='text-xl flex gap-5 items-center'>
+          Total Donation{' '}
+          <span className='w-24 h-2 bg-[#FF444A] block rounded'></span>
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Statistics;
