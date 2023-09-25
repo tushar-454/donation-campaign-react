d215e60f4988a82789736c3b941baf523cabc5e7
tushar-454 <imtushar454@gmail.com>
2023-09-25 09:09:23 +0600

    click donate button add card details id on localstorage function successfully

--- src/Components/DonationCardDetails/DonationCardDetails.jsx
+++ src/Components/DonationCardDetails/DonationCardDetails.jsx
@@ -1,16 +1,44 @@
 import { useLoaderData, useParams } from 'react-router-dom';
-
+import swal from 'sweetalert';
 const DonationCardDetails = () => {
   const cards = useLoaderData();
-  const { id } = useParams();
+  const { id, category } = useParams();
   const card = cards.find((card) => card.id === parseInt(id));
   const { picture, title, description, donate, text_color } = card;
+  const handleDonate = () => {
+    let donateList = [];
+
+    if (!localStorage.getItem('donateId')) {
+      donateList.push(id);
+      localStorage.setItem('donateId', JSON.stringify(donateList));
+      swal(
+        'Donation Successfull!',
+        `You donate on ${category} category about $${donate}`,
+        'success'
+      );
+    } else {
+      donateList = JSON.parse(localStorage.getItem('donateId'));
+      // if (donateList.includes(id)) {
+      //   return;
+      // }
+      donateList.push(id);
+      localStorage.setItem('donateId', JSON.stringify(donateList));
+      swal(
+        'Donation Successfull!',
+        `You donate on ${category} category about $${donate}`,
+        'success'
+      );
+    }
+  };
   return (
     <div className='container mx-auto px-4 -z-10'>
       <div className='my-10'>
         <div className='cardPicture relative'>
           <img src={picture} className='w-full rounded-lg' />
-          <div className='priceOverlays flex items-center absolute bottom-0 w-full h-[10vh] bg-[#00000050] rounded-br-lg rounded-bl-lg'>
+          <div
+            className='priceOverlays flex items-center absolute bottom-0 w-full h-[10vh] bg-[#00000050] rounded-br-lg rounded-bl-lg cursor-pointer'
+            onClick={handleDonate}
+          >
             <h1
               className='text-white px-4 py-2 ml-5 rounded-lg inline-block'
               style={{ backgroundColor: text_color }}
