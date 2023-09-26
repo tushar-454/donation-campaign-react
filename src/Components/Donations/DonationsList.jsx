64af63b115e8dc3e45613529d0dfe22db52fb755
tushar-454 <imtushar454@gmail.com>
2023-09-26 15:27:32 +0600

    total donate money track user and clear all donate item features added

--- src/Components/Donations/DonationsList.jsx
+++ src/Components/Donations/DonationsList.jsx
@@ -7,6 +7,7 @@ import DonationListCardDetails from './DonationListCardDetails';
 const Donations = () => {
   const [donateCards, setDonateCards] = useState([]);
   const [seemore, setSeeMore] = useState(false);
+  const [total, setTotal] = useState(0);
   const [slice, setSlice] = useState(4);
   const donations = useLoaderData();
   const donateId = JSON.parse(localStorage.getItem('donateId'));
@@ -15,10 +16,18 @@ const Donations = () => {
   );
   useEffect(() => {
     setDonateCards(donateIdList);
+    const totalDonate = donateIdList
+      .map((item) => item.price)
+      .reduce((acc, cur) => acc + cur, 0);
+    setTotal(totalDonate);
   }, []);
   const handleSeeMore = () => {
     setSeeMore(!seemore);
     setSlice(donateIdList.length);
+  };
+  const handleClear = () => {
+    setDonateCards([]);
+    localStorage.setItem('donateId', '[]');
   };
   return (
     <div>
@@ -32,6 +41,19 @@ const Donations = () => {
             </>
           ) : (
             <>
+              <div className='my-5'>
+                <div className='totalDonate space-y-3'>
+                  <h1 className='text-3xl font-medium'>
+                    Your total donate: ${total.toFixed(2)}
+                  </h1>
+                  <button
+                    onClick={handleClear}
+                    className='text-xl px-3 py-2 bg-purple-500 text-white rounded-lg'
+                  >
+                    Clear all donation
+                  </button>
+                </div>
+              </div>
               <div className='grid gap-5 grid-cols-1 lg:grid-cols-2'>
                 {seemore
                   ? donateCards
